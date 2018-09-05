65553b882c1f47c3807fedd95f6a2644b727d4ad
Rodrigo Varas <rvaras@ceapro.cl>
2018-09-05 14:53:27 -0300

    fin rev17

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict'
-// var https = require('https');
-// var fs = require('fs');
+ var https = require('https');
+ var fs = require('fs');
 
 var mongoose = require('mongoose');
 var app = require('./app');
@@ -9,15 +9,14 @@ var port = process.env.PORT || 3789;
 
 
 //Certificados
-// var key = fs.readFileSync('/etc/nginx/certs/centropiecmds.cl/key.pem');
-// var cert = fs.readFileSync( '/etc/nginx/certs/centropiecmds.cl/fullchain.pem' );
-// var ca = fs.readFileSync( '/etc/nginx/certs/centropiecmds.cl/ca.pem' );
 
-// var options = {
-// 	key: key,
-// 	cert: cert,
-// 	ca: ca
-// };
+var credentials = {
+	ca: fs.readFileSync(__dirname+"/etc/nginx/certs/centropiecmds.cl/ca.pem", 'utf8'), //la certification authority o CA
+	key: fs.readFileSync(__dirname+"/etc/nginx/certs/centropiecmds.cl/fullchain.pem", 'utf8'), //la clave SSL, que es el primer archivo que generamos ;)
+	cert: fs.readFileSync(__dirname+"/ssl/certificate.crt", 'utf8') //el certificado
+};
+
+ 
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/adminpie',(err,res) => {
@@ -28,9 +27,7 @@ if (err) {
 
 
 
- //    https.createServer(options, app).listen(port, function(){
- //    console.log("Servidor de adminpie escuchando en http://localhost:" + port);
-	// });
+ 
 
 	app.listen(port, function(){
     console.log("Servidor de adminpie escuchando en http://localhost:" + port);

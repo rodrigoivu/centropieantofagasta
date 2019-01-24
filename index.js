3de5cda40bb56fb70f1af079f0574a6c17688202
System Administrator <root@iMac-de-Rodrigo.local>
2019-01-24 16:43:31 -0300

    ssl cuarto

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ var port = process.env.PORT || 3789;
 //Certificados
 
 var credentials = {
-	ca: fs.readFileSync("/etc/nginx/certs/centropiecmds.cl/ca.pem"), //la certification authority o CA
-	key: fs.readFileSync("/etc/nginx/certs/centropiecmds.cl/key.pem"), //la clave SSL, que es el primer archivo que generamos ;)
-	cert: fs.readFileSync("/etc/nginx/certs/centropiecmds.cl/fullchain.pem") //el certificado
+	//ca: fs.readFileSync("/etc/letsencrypt/live/centropiecmds.cl/ca.pem"), //la certification authority o CA
+	key: fs.readFileSync("/etc/letsencrypt/live/centropiecmds.cl/privkey.pem"), //la clave SSL, que es el primer archivo que generamos ;)
+	cert: fs.readFileSync("/etc/letsencrypt/live/centropiecmds.cl/fullchain.pem") //el certificado
 	// //cert: fs.readFileSync("/etc/nginx/certs/centropiecmds.cl/fullchain.pem", 'utf8')
 };
 
@@ -26,15 +26,12 @@ if (err) {
 }else{
 	console.log("La conexión a la base de datos está funcionando correctamente...");
 
-
-
-   
- //    https.createServer(credentials, app).listen(port, function(){
- //    console.log("Servidor de adminpie escuchando en http://localhost:" + port);
-	// });
-
-	app.listen(port, function(){
+    https.createServer(credentials, app).listen(port, function(){
     console.log("Servidor de adminpie escuchando en http://localhost:" + port);
 	});
+
+	// app.listen(port, function(){
+ //    console.log("Servidor de adminpie escuchando en http://localhost:" + port);
+	// });
 }
 });
